429df81d5d6eba35adca41fbb2572668a1038e13
Shawn <shawnm0705@gmail.com>
2021-07-06 10:47:24 +0800

    [CORE-4569]

--- src/helper.ts
+++ src/helper.ts
@@ -25,7 +25,10 @@ export function parse(token: string, envVariables: any, service: string, algorit
       if (envVariables.ENV &&
         ['live', 'prod', 'production'].includes(envVariables.ENV.toLowerCase()) &&
         !payload.exp) {
-        throw "jwt token doesn't have expire time";
+        throw {
+          name: "jwt payload error",
+          message: "jwt token doesn't have expire time"
+        };
       }
       contents = payload;
     },
